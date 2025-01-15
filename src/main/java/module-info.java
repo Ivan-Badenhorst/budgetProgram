3d0a96eac0d642cf31f9483ec4d4361f4d249c3a
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2025-01-15 14:40:01 +0100

    initial project commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.personal.budget {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.personal.budget to javafx.fxml;
+    exports com.personal.budget;
+}
