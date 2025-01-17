0b8926728ea4bbede78695c7185b5d16411fba2b
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2025-01-17 16:24:45 +0100

    Basic setup complete for scene management - with dummy guis currently

--- src/main/java/com/personal/budget/HelloController.java
+++ src/main/java/com/personal/budget/HelloController.java
@@ -1,5 +1,6 @@
 package com.personal.budget;
 
+import controllers.SceneManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -8,7 +9,8 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onGoNextClick() {
+        SceneManager sceneManager = SceneManager.getInstance();
+        sceneManager.showScene("second");
     }
 }
