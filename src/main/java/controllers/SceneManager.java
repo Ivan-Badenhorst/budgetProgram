fedc92e7f9ff0347ff644e5d4a1da8932a94a804
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2025-01-17 16:07:56 +0100

    SceneManager basic doc

--- src/main/java/controllers/SceneManager.java
+++ src/main/java/controllers/SceneManager.java
@@ -9,6 +9,12 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * SceneManager is responsible for managing different scenes in a JavaFX application.
+ * SceneManager is a singleton.
+ * It allows for loading and switching between scenes during the application's runtime.
+ * This class implements the Singleton pattern to ensure only one instance of the SceneManager exists.
+ */
 public class SceneManager {
     private static SceneManager instance;
     private Stage primaryStage;
@@ -23,10 +29,22 @@ public class SceneManager {
         return  instance;
     }
 
+    /**
+     * Sets the primary stage where scenes will be displayed.
+     *
+     * @param primaryStage the primary stage of the application.
+     */
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
+    /**
+     * Loads a scene from an FXML file and stores it in the scenes map under a given name.
+     *
+     * @param name the name of the scene to be stored.
+     * @param fxmlPath the path to the FXML file for the scene.
+     * @throws IOException if the FXML file cannot be loaded.
+     */
     public void loadScene(String name, String fxmlPath) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
         Parent root = loader.load();
@@ -34,6 +52,12 @@ public class SceneManager {
         scenes.put(name, scene);
     }
 
+    /**
+     * Displays the scene corresponding to the provided name on the primary stage.
+     * If no scene with this name has been loaded, nothing happens
+     *
+     * @param name the name of the scene to be displayed.
+     */
     public void showScene(String name){
 
         if(primaryStage != null && scenes.containsKey(name)){
