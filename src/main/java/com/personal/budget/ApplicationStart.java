2c0cb624e7ee6b49493fc59f9ff528345352589a
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2025-01-17 16:21:01 +0100

    change name of starting class

--- src/main/java/com/personal/budget/ApplicationStart.java
+++ src/main/java/com/personal/budget/ApplicationStart.java
@@ -2,13 +2,11 @@ package com.personal.budget;
 
 import controllers.SceneManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ApplicationStart extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         SceneManager sceneManager = SceneManager.getInstance();
