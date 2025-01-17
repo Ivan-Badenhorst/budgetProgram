659603c6fa0e15faa2a0da72a6e3ae3f8568c628
Ivan Badenhorst <ivan.badenhorst@student.kuleuven.be>
2025-01-17 13:59:38 +0100

    Added CategoriesDAO

---
+++ src/main/java/DB/CategoriesDAO.java
@@ -0,0 +1,25 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CategoriesDAO {
+
+    public boolean insertCategory(String value){
+        String query = "INSERT INTO tester (value) VALUES (?)";
+
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, value);
+            int rows = preparedStatement.executeUpdate();
+            System.out.println("Inserted " + rows + " row(s).");
+            return true;
+
+        } catch (SQLException e) {
+            System.err.println("Error inserting value: " + e.getMessage());
+        }
+        return false;
+    }
+
+}
